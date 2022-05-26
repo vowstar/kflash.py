103469a23ebbd8fa85b62491fb0a25835404f8bb
Huang Rui <vowstar@gmail.com>
2022-05-26 11:31:33 +0800

    kflash.py: remove the backports.tempfile
    Issue: https://github.com/vowstar/kflash.py/issues/3
    Signed-off-by: Huang Rui <vowstar@gmail.com>

--- kflash.py
+++ kflash.py
@@ -12,7 +12,7 @@ import binascii
 import hashlib
 import argparse
 import math
-import zipfile, tempfile, backports.tempfile
+import zipfile, tempfile
 import json
 import re
 import os
@@ -88,6 +88,90 @@ class KFlash:
             FMT_BINARY = 0
             FMT_ELF = 1
             FMT_KFPKG = 2
+
+        class TemporaryDirectory(object):
+            """Create and return a temporary directory.  This has the same
+            behavior as mkdtemp but can be used as a context manager.  For
+            example:
+
+                with TemporaryDirectory() as tmpdir:
+                    ...
+
+            Upon exiting the context, the directory and everything contained
+            in it are removed.
+            """
+            import warnings as _warnings
+            import os as _os
+
+            def __init__(self, suffix="", prefix="tmp", dir=None):
+                self._closed = False
+                self.name = None # Handle mkdtemp raising an exception
+                from tempfile import mkdtemp
+                self.name = mkdtemp(suffix, prefix, dir)
+
+            def __repr__(self):
+                return "<{} {!r}>".format(self.__class__.__name__, self.name)
+
+            def __enter__(self):
+                return self.name
+
+            def cleanup(self, _warn=False):
+                if self.name and not self._closed:
+                    try:
+                        self._rmtree(self.name)
+                    except (TypeError, AttributeError) as ex:
+                        # Issue #10188: Emit a warning on stderr
+                        # if the directory could not be cleaned
+                        # up due to missing globals
+                        if "None" not in str(ex):
+                            raise
+                        print("ERROR: {!r} while cleaning up {!r}".format(ex, self,),
+                            file=_sys.stderr)
+                        return
+                    self._closed = True
+                    if _warn:
+                        self._warn("Implicitly cleaning up {!r}".format(self),
+                                ResourceWarning)
+
+            def __exit__(self, exc, value, tb):
+                self.cleanup()
+
+            def __del__(self):
+                # Issue a ResourceWarning if implicit cleanup needed
+                self.cleanup(_warn=True)
+
+            # XXX (ncoghlan): The following code attempts to make
+            # this class tolerant of the module nulling out process
+            # that happens during CPython interpreter shutdown
+            # Alas, it doesn't actually manage it. See issue #10188
+            _listdir = staticmethod(_os.listdir)
+            _path_join = staticmethod(_os.path.join)
+            _isdir = staticmethod(_os.path.isdir)
+            _islink = staticmethod(_os.path.islink)
+            _remove = staticmethod(_os.remove)
+            _rmdir = staticmethod(_os.rmdir)
+            _warn = _warnings.warn
+
+            def _rmtree(self, path):
+                # Essentially a stripped down version of shutil.rmtree.  We can't
+                # use globals because they may be None'ed out at shutdown.
+                for name in self._listdir(path):
+                    fullname = self._path_join(path, name)
+                    try:
+                        isdir = self._isdir(fullname) and not self._islink(fullname)
+                    except OSError:
+                        isdir = False
+                    if isdir:
+                        self._rmtree(fullname)
+                    else:
+                        try:
+                            self._remove(fullname)
+                        except OSError:
+                            pass
+                try:
+                    self._rmdir(path)
+                except OSError:
+                    pass
 
         # AES is from: https://github.com/ricmoo/pyaes, Copyright by Richard Moore
         class AES:
@@ -1600,7 +1684,7 @@ class KFlash:
 
         if file_format == ProgramFileFormat.FMT_KFPKG:
             KFlash.log(INFO_MSG,"Extracting KFPKG ... ", BASH_TIPS['DEFAULT'])
-            with tempfile.TemporaryDirectory() as tmpdir:
+            with TemporaryDirectory() as tmpdir:
                 try:
                     with zipfile.ZipFile(args.firmware) as zf:
                         zf.extractall(tmpdir)
